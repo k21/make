c66b3fce998d0bf026626b860c2912bfa8fa80da
Jakub Zika <j@kub-zika.cz>
2014-02-22 21:46:33 +0100

    Simplify xmalloc implementation

--- xmalloc.c
+++ xmalloc.c
@@ -1,25 +1,22 @@
 #define	_POSIX_C_SOURCE	200809L
 
-#include <stdio.h>
-
+#include "error.h"
 #include "xmalloc.h"
+
+static void *check_not_null(void *ptr) {
+	if (!ptr) {
+		fatal_error("Out of memory");
+	}
+
+	return (ptr);
+}
 
 void *xmalloc(size_t size) {
 	void *result = malloc(size);
-	if (!result) {
-		fprintf(stderr, "Error: Out of memory\n");
-		abort();
-	}
-
-	return (result);
+	return (check_not_null(result));
 }
 
 void *xrealloc(void *ptr, size_t size) {
 	void *result = realloc(ptr, size);
-	if (!result) {
-		fprintf(stderr, "Error: Out of memory\n");
-		abort();
-	}
-
-	return (result);
+	return (check_not_null(result));
 }
