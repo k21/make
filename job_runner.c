8cd3c40cdffa5b87b02eb910ed7ed3471c91fada
Jakub Zika <j@kub-zika.cz>
2014-02-28 17:20:19 +0100

    Check exit value of launched jobs

--- job_runner.c
+++ job_runner.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -48,6 +49,24 @@ static pid_t start_job(
 	return (pid);
 }
 
+static void wait_for_jobs() {
+	while (1) {
+		int stat;
+		pid_t pid;
+
+		pid = wait(&stat);
+
+		if (pid == -1) {
+			if (errno == ECHILD) {
+				/* No more children to wait for, done */
+				break;
+			}
+
+			assert(errno == EINTR);
+		}
+	}
+}
+
 int run_jobs(struct graph *graph, struct dict *macros, size_t max_jobs) {
 	size_t running_jobs = 0;
 	struct graph_node *node;
@@ -57,6 +76,7 @@ int run_jobs(struct graph *graph, struct dict *macros, size_t max_jobs) {
 	struct list_item **commands;
 
 	int any_job_launchable = 0;
+	int error = 0;
 
 	assert(max_jobs > 0);
 
@@ -95,9 +115,16 @@ int run_jobs(struct graph *graph, struct dict *macros, size_t max_jobs) {
 
 		if (running_jobs > 0) {
 			int stat;
-			pid_t pid;
+			pid_t pid = -1;
+			int terminated;
 
-			pid = wait(&stat);
+			while (pid == -1) {
+				pid = wait(&stat);
+				if (pid == -1) {
+					assert(errno == EINTR);
+				}
+			}
+
 			for (i = 0; i < running_jobs; ++i) {
 				if (pids[i] == pid) {
 					break;
@@ -105,7 +132,26 @@ int run_jobs(struct graph *graph, struct dict *macros, size_t max_jobs) {
 			}
 			assert(i != running_jobs);
 
-			/* TODO: check stat */
+			terminated = 0;
+
+			if (WIFEXITED(stat)) {
+				terminated = 1;
+				error = (WEXITSTATUS(stat) != 0);
+			}
+
+			if (WIFSIGNALED(stat)) {
+				terminated = 1;
+				error = 1;
+			}
+
+			if (error) {
+				wait_for_jobs();
+				break;
+			}
+
+			if (!terminated) {
+				continue;
+			}
 
 			pids[i] = 0;
 			commands[i] = list_next(commands[i]);
@@ -130,5 +176,5 @@ int run_jobs(struct graph *graph, struct dict *macros, size_t max_jobs) {
 	free(nodes);
 	free(commands);
 
-	return (0);
+	return (error ? 2 : 0);
 }
